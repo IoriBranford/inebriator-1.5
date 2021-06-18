2dad41a4c069ff44a0a72a0871cee4bb51edf3ef
IoriBranford <ioribranford@gmail.com>
2021-06-18 15:50:06 -0700

    Add camera code module.

---
+++ scripts/Camera.lua
@@ -0,0 +1,56 @@
+local Physics = require "Physics"
+local Units = require "Units"
+local Camera = {}
+
+Camera.Prefab = {
+	id = "camera",
+	width = 240, height = 320,
+	x = 0, y = 0,
+	velx = 0, vely = -1,
+	module = "Camera",
+	start = "start",
+	think = "think",
+	bodytype = "dynamic"
+}
+
+function Camera.start(camera)
+	camera.rectfixture = Physics.addRectangleFixture(camera.id, camera.width, camera.height, camera.width/2, camera.height/2)
+	camera.rectfixture:setSensor(true)
+	local offscreenmargin = 16
+	camera.offscreeneater = Physics.addChainFixture(camera.id, true,
+		-offscreenmargin, -offscreenmargin,
+		camera.width+offscreenmargin, -offscreenmargin,
+		camera.width+offscreenmargin, camera.height+offscreenmargin,
+		-offscreenmargin, camera.height+offscreenmargin)
+	camera.offscreeneater:setSensor(true)
+end
+
+function Camera.think(camera)
+	Camera.doCollisions(camera)
+end
+
+function Camera.doCollisions(camera)
+	local body = camera.body
+	local contacts = body:getContacts()
+	local offscreeneater = camera.offscreeneater
+	for i = 1, #contacts do
+		local contact = contacts[i]
+		if contact:isTouching() then
+			local f1, f2 = contact:getFixtures()
+			local b1, b2 = f1:getBody(), f2:getBody()
+			if b2 == body then
+				f1, f2 = f2, f1
+				b1, b2 = b2, b1
+			end
+			if f1 == offscreeneater then
+				local id2 = b2:getUserData()
+				local u2 = Units.get(id2)
+				if u2 and not u2.think then
+					Units.remove(u2)
+				end
+			end
+		end
+	end
+end
+
+return Camera
